88ab5b8c5ca31d50488ebbc215f77624ffb5876d
ShawnXxy <shawnxxy@hotmail.com>
2017-08-04 19:35:11 +0800

    LED BUTTON TEST

--- hello-ruff/src/index.js
+++ hello-ruff/src/index.js
@@ -1,12 +1,45 @@
 'use strict';
 
+// Use http to report led's status
+var http = require('http');
+var option = {
+    host: 'httpbin.org', // httpbin is a setuponline http service
+    path: '/post',
+    method: 'POST',
+    headers: {
+
+    }
+};
+function postState(state) {
+    options.headers['Content-Length'] = state.length;
+    var req = http.request(options, function(res) {
+        res.on('data', function(chunk) {
+            console.log('BODY: ' + chunk);
+        });
+    });
+    req.write(state);
+    req.end();
+}
+
+
 $.ready(function (error) {
     if (error) {
         console.log(error);
         return;
     }
 
-    $('#led-r').turnOn();
+    // lightup "#led-r" when #button is pressed
+    $('#button').on('push', function() {
+        console.log('Button pushed.');
+        $('#led-r').turnOn();
+    });
+    // lightdown '#led-r' when #button is released
+    $('#button').on('release', function() {
+        console.log('Button released.');
+        $('#led-r').turnOff();
+    });
+
+    // $('#led-r').turnOn();
 });
 
 $.end(function () {
